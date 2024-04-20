9e70fbbde7c5eada22171ddbc22e7371e3cfd0e9
misalibaytb <misaliba@michlip.eu>
2024-04-20 16:28:42 +0200

    Update admin.ts

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -3,15 +3,11 @@ import { config } from '@/index';
 import db from '@/utils/db';
 const app = express.Router();
 
-app.get('/system', async (req: express.Request, res: express.Response) => {
-    const nodes = await db.query('SELECT id,name,plan,sort FROM nodes');
+app.get('/admin/images', async (req: express.Request, res: express.Response) => {
     const images = await db.query('SELECT id,name,sort FROM images');
-    res.json({
-        name: config.name,
-        logo: config.logo,
-        nodes: nodes,
-        images: images
+    res.send({
+        data: images
     })
-});
+})
 
 export default app;
