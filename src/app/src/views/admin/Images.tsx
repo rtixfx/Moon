6b3cafbac942e0f48e23bf597db52c3e7f16ae01
misalibaytb <misaliba@michlip.eu>
2024-04-20 16:28:44 +0200

    Update Images.tsx

--- src/app/src/views/admin/Images.tsx
+++ src/app/src/views/admin/Images.tsx
@@ -4,46 +4,40 @@ import SystemContenxt from '@/state/system';
 
 export default () => {
     const system = SystemContenxt.useStoreState((state) => state.system);
-    const [images, setImages] = useState([]);
+    const [images, setImages] = useState(null);
     useEffect(() => {
-        axios.get('/admin/images').then((res) => {
-            setImages(res.data.data);
-        }).catch((e) => {
-            console.error(e);
-        })
+        setTimeout(() => {
+            axios.get('/admin/images').then((res) => {
+                console.log(res.data);
+                setImages(res.data.data);
+            }).catch((e) => {
+                console.error(e);
+            })
+        }, 1000)
     }, [])
 
     return (
         <div className="dashboard">
-            <div className="dashboard__servers-list">
-                <h4 className="dashboard__servers-list-title">Images</h4>
-                <div className="dashboard__servers-list-table-responsive">
+            <h4 className="dashboard__servers-list-title">Images</h4>
+            {images === null && <div className="dashboard__loader">
+                <div className="loader__circle"></div>
+                <h2>Loading...</h2>
+            </div>}
+            <div className="servers__row">
+                {
+                    images !== null &&
+                    images.map((image: any) => {
+                        return (
+                            <div className="servers__row__item">
+                                <h4 className="servers__row__item-title">{image.name}</h4>
+                                <h5 className="servers__row__item-subtitle">Image {image.id}</h5>
+                                <p><i className="fas fa-sort-numeric-up"></i> Sort: {image.sort}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
 
-                    <table className="dashboard__servers-list-table">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                images.map((image: any, index: any) => (
-                                    <tr key={index}>
-                                        <td>{image.id}</td>
-                                        <td>{image.name}</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                    {images.length === 0 && <div className="dashboard__loader">
-                        <div className="loader__circle"></div>
-                        <h2>Loading...</h2>
-                    </div>}
-                </div>
-            </div >
         </div>
     )
 }
