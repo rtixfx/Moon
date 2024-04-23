eae1910bd32ba112212d354236bd5e34d46db9a8
misalibaytb <misaliba@michlip.eu>
2024-04-23 15:12:53 +0200

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,9 +1,5 @@
 const spawn = require('child_process').spawn;
 process.stdin.resume();
-const axios = require('axios');
-const download = require('download');
-const admZip = require('adm-zip');
-const fs = require('fs');
 
 const startMainProcess = async () => {
     const main = spawn('npx', ['--yes', 'ts-node', '-r', 'tsconfig-paths/register', './src/index.ts'], {
